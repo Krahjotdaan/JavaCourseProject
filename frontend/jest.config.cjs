1a25bf5211a180d2509c4ec15239e6e558525e64
Artem Ostapenko <a.ostapenko@edu.centraluniversity.ru>
2025-05-16 11:36:14 +0300

    Test: removed test for frontend

--- frontend/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.js?$': 'babel-jest',
-  },
-  setupFilesAfterEnv: ['<rootDir>/src/test/setupTests.js'],
-};
